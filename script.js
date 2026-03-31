9d313ff1f735ead9f27bc5e228a3b9256df608d3
yusufavsarsgu-lab <yusufavsarsgu@gmail.com>
2026-03-31 20:27:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+menuToggle.addEventListener('click', ()=>{
+    menu.classList.toggle('active');
+    menu.style.display = menu.classList.contains('active')?'flex':'none';
+});
