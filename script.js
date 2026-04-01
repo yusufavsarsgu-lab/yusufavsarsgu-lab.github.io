50c603cbc1eeffec94114a98a17be55b2291e3ae
yusufavsarsgu-lab <yusufavsarsgu@gmail.com>
2026-04-01 22:59:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-﻿const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-menuToggle.addEventListener('click', ()=>{
-    menu.classList.toggle('active');
-    menu.style.display = menu.classList.contains('active')?'flex':'none';
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const skillFills = document.querySelectorAll('.skill-fill');
+    skillFills.forEach(fill => {
+        const width = fill.getAttribute('data-width');
+        setTimeout(() => fill.style.width = width, 300);
+    });
 });
